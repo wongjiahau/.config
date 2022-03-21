8558cdcb7bee750de198eeed767e698ca9aaa515
Wong Jia Hau <hou32hou@gmail.com>
2022-03-21 02:46:21 +0000

    chore(config.fish): format with fish_indent

--- fish/config.fish
+++ fish/config.fish
@@ -9,16 +9,16 @@ set -gx LC_ALL en_US.UTF-8
 # Solve SSH cannot open connection issue
 # Refer https://gist.github.com/gerbsen/5fd8aa0fde87ac7a2cae
 setenv SSH_ENV $HOME/.ssh/environment
-function start_agent                                                                                                                                                                    
+function start_agent
     echo "Initializing new SSH agent ..."
-    ssh-agent -c | sed 's/^echo/#echo/' > $SSH_ENV
+    ssh-agent -c | sed 's/^echo/#echo/' >$SSH_ENV
     echo "succeeded"
-    chmod 600 $SSH_ENV 
-    . $SSH_ENV > /dev/null
+    chmod 600 $SSH_ENV
+    . $SSH_ENV >/dev/null
     ssh-add
 end
-function test_identities                                                                                                                                                                
-    ssh-add -l | grep "The agent has no identities" > /dev/null
+function test_identities
+    ssh-add -l | grep "The agent has no identities" >/dev/null
     if [ $status -eq 0 ]
         ssh-add
         if [ $status -eq 2 ]
@@ -26,20 +26,19 @@ function test_identities
         end
     end
 end
-if [ -n "$SSH_AGENT_PID" ] 
-    ps -ef | grep $SSH_AGENT_PID | grep ssh-agent > /dev/null
+if [ -n "$SSH_AGENT_PID" ]
+    ps -ef | grep $SSH_AGENT_PID | grep ssh-agent >/dev/null
     if [ $status -eq 0 ]
         test_identities
-    end  
+    end
 else
     if [ -f $SSH_ENV ]
-        . $SSH_ENV > /dev/null
-    end  
-    ps -ef | grep $SSH_AGENT_PID | grep -v grep | grep ssh-agent > /dev/null
+        . $SSH_ENV >/dev/null
+    end
+    ps -ef | grep $SSH_AGENT_PID | grep -v grep | grep ssh-agent >/dev/null
     if [ $status -eq 0 ]
         test_identities
-    else 
+    else
         start_agent
-    end  
+    end
 end
-
