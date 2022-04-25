5f3029b6f62105210ae0faa95e2a954b8b46c6ef
Wong Jia Hau <hou32hou@gmail.com>
2022-04-25 10:14:47 +0800

    fix(nvim): tree-sitter deprecated option Specifically, the `ensure_installed = "maintained"`.

--- nvim/init.lua
+++ nvim/init.lua
@@ -19,7 +19,7 @@ require('nvim-treesitter.configs').setup({
   autotag = {
     enable = true
   },
-  ensure_installed = "maintained", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+  ensure_installed = "all",
   highlight = {
     enable = true,              -- false will disable the whole extension
     disable = { },  -- list of language that will be disabled
