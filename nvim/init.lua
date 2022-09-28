7946ec84b9dfaa4e0446c0b2bf49376b2fc8548b
Jia Hau Wong <wongjiahau@Jias-MBP.realtek>
2022-09-28 21:27:43 +0800

    feat(nvim): use mason to handle LSP server installation

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,98 +1,96 @@
-require('plugins')
+require("plugins")
 local o = vim.o
-local wo = vim.wo
-local bo = vim.bo
 local g = vim.g
 local function nnoremap(key, cmd)
-    vim.api.nvim_set_keymap('n', key, cmd, {noremap = true})
+	vim.api.nvim_set_keymap("n", key, cmd, { noremap = true })
 end
+
 local function vnoremap(key, cmd)
-    vim.api.nvim_set_keymap('v', key, cmd, {noremap = true})
+	vim.api.nvim_set_keymap("v", key, cmd, { noremap = true })
 end
 
 -- Set leader
 g.mapleader = " "
 
 -- Tree sitter settings
-require('nvim-treesitter.configs').setup({
-    autotag = {enable = true},
-    ensure_installed = "all",
-    highlight = {
-        enable = true, -- false will disable the whole extension
-        disable = {} -- list of language that will be disabled
-    }
+require("nvim-treesitter.configs").setup({
+	autotag = { enable = true },
+	ensure_installed = "all",
+	highlight = {
+		enable = true, -- false will disable the whole extension
+		disable = {}, -- list of language that will be disabled
+	},
 })
 
 -- Autopair settings
-require('nvim-autopairs').setup({})
+require("nvim-autopairs").setup({})
 
 -- Make sure all types of *.graphql files get syntax highlighting.
 -- This is necessary for coc-prettier to work
-vim.cmd('autocmd BufNewFile,BufRead *.graphql set ft=graphql')
+vim.cmd("autocmd BufNewFile,BufRead *.graphql set ft=graphql")
 
 -- lualine settings
-require('lualine').setup({
-    options = {theme = 'ayu_mirage'},
-    sections = {
-        lualine_a = {{'filename', path = 1}},
-        lualine_b = {'g:coc_status'},
-        lualine_c = {{'diagnostics', sources = {'nvim_lsp'}}},
-        lualine_x = {},
-        lualine_y = {'progress'},
-        lualine_z = {'branch'}
-    }
+require("lualine").setup({
+	options = { theme = "ayu_mirage" },
+	sections = {
+		lualine_a = { { "filename", path = 1 } },
+		lualine_b = { "g:coc_status" },
+		lualine_c = { { "diagnostics", sources = { "nvim_lsp" } } },
+		lualine_x = {},
+		lualine_y = { "progress" },
+		lualine_z = { "branch" },
+	},
 })
 
 -- diffview settings
-require('diffview').setup({
-    file_panel = {win_config = {tree_options = {flatten_dirs = false}}}
+require("diffview").setup({
+	file_panel = { win_config = { tree_options = { flatten_dirs = false } } },
 })
 
 -- telescope settings
-require('telescope').setup({defaults = {preview = false}})
+require("telescope").setup({ defaults = { preview = false } })
 
 -- https://github.com/alvarosevilla95/luatab.nvim
-require('luatab').setup({})
-
+require("luatab").setup({})
 
 -- key mappings
-nnoremap('<CR>', ':wa<CR>')
-nnoremap('<C-j>', ':cnext<CR>')
-nnoremap('<C-k>', ':cprev<CR>')
--- Open [g]it dashboard 
-nnoremap('<Leader>g', ':tab Git<CR>')
--- Open [G]it diffs 
-nnoremap('<Leader>G', ':DiffviewOpen<CR>')
--- For opening Git [d]iff of current file vertically 
-nnoremap('<Leader>d', ':vert Gdiff<CR>')
+nnoremap("<CR>", ":wa<CR>")
+nnoremap("<C-j>", ":cnext<CR>")
+nnoremap("<C-k>", ":cprev<CR>")
+-- Open [g]it dashboard
+nnoremap("<Leader>g", ":tab Git<CR>")
+-- Open [G]it diffs
+nnoremap("<Leader>G", ":DiffviewOpen<CR>")
+-- For opening Git [d]iff of current file vertically
+nnoremap("<Leader>d", ":vert Gdiff<CR>")
 -- [s]earch and replace
-nnoremap('<leader>s', ':lua require("spectre").open() <CR>')
+nnoremap("<leader>s", ':lua require("spectre").open() <CR>')
 -- [s]earch current highlighted word
-vnoremap('<leader>s', ':lua require("spectre").open_visual() <CR>')
+vnoremap("<leader>s", ':lua require("spectre").open_visual() <CR>')
 -- Open file search (follows VSCode Cmd+p)
-nnoremap('<Leader>p', ':Telescope git_files<CR>')
+nnoremap("<Leader>p", ":Telescope git_files<CR>")
 -- Open command pallete (follows VSCode Cmd+shift+p)
-nnoremap('<Leader>P', ':Telescope commands<CR>')
+nnoremap("<Leader>P", ":Telescope commands<CR>")
 -- Open registers search
-nnoremap('<Leader>"', ':Telescope registers<CR>')
+nnoremap('<Leader>"', ":Telescope registers<CR>")
 -- Open [b]uffer search
-nnoremap('<Leader>b', ':Telescope buffers<CR>')
+nnoremap("<Leader>b", ":Telescope buffers<CR>")
 -- Global [f]ind
-nnoremap('<Leader>f', ':Telescope live_grep<cr>')
+nnoremap("<Leader>f", ":Telescope live_grep<cr>")
 -- Open current file [h]istory
-nnoremap('<Leader>h', ':DiffviewFileHistory %<CR>')
+nnoremap("<Leader>h", ":DiffviewFileHistory %<CR>")
 -- Toggle hop.nvim
-nnoremap('<Leader>t', ':HopWord<CR>')
+nnoremap("<Leader>t", ":HopWord<CR>")
 
 -- vim settings
-o.clipboard = o.clipboard .. 'unnamedplus'
+o.clipboard = o.clipboard .. "unnamedplus"
 o.number = true
 o.autowrite = true
 o.smartcase = true
 o.ignorecase = true
 o.tabstop = 2
 o.shiftwidth = 2
-o.shell = 'fish'
+o.shell = "fish"
 o.expandtab = true
 o.wrap = false
 o.swapfile = false
@@ -101,153 +99,224 @@ o.cursorcolumn = true
 o.hlsearch = true
 o.incsearch = true
 o.ttimeoutlen = 10
-o.mouse = 'a'
-o.inccommand = 'nosplit' -- For viewing live substitution
+o.mouse = "a"
+o.inccommand = "nosplit" -- For viewing live substitution
 o.hidden = true -- to ensure terminal remains alive
 o.relativenumber = true
 
 -- colorscheme
 vim.o.background = "light"
 vim.g.vscode_transparent = 1
-vim.cmd [[colorscheme vscode]]
+vim.cmd([[colorscheme vscode]])
 
 -- tabline colors
-vim.cmd [[
+vim.cmd([[
 hi TabLine guibg=#A89984
 hi TabLineFill guibg=#3C3836
-]]
+]])
 
 -- floating windows color
-vim.cmd [[
+vim.cmd([[
 hi NormalFloat guibg=#fbf1c7
 hi Visual guibg=yellow
-]]
-
+]])
 
 -- Treat dash as part of a word
 -- Refer https://vi.stackexchange.com/a/13813/31905
-vim.cmd [[
+vim.cmd([[
 set iskeyword+=-
-]]
+]])
 
 -- Auto format on save
 -- Refer https://github.com/sbdchd/neoformat#basic-usage
-vim.cmd [[
+vim.cmd([[
 augroup fmt
   autocmd!
   autocmd BufWritePre * undojoin | Neoformat
 augroup END
-]]
+]])
 
 -- https://github.com/lewis6991/gitsigns.nvim
-require('gitsigns').setup()
+require("gitsigns").setup()
 
 -- https://github.com/kyazdani42/nvim-tree.lua
 vim.g.loaded = 1
 vim.g.loaded_netrwPlugin = 1
 require("nvim-tree").setup()
-nnoremap('<Leader>n', ':NvimTreeFindFileToggle<CR>')
+nnoremap("<Leader>n", ":NvimTreeFindFileToggle<CR>")
 
+-- https://github.com/williamboman/mason.nvim#installation
+require("mason").setup({})
+
+-- https://github.com/williamboman/mason-lspconfig.nvim#setup
+require("mason-lspconfig").setup()
+
+-- https://github.com/WhoIsSethDaniel/mason-tool-installer.nvim
+require("mason-tool-installer").setup({
+	ensure_installed = {
+		"lua-language-server",
+		"prettierd",
+		"rust-analyzer",
+		{ "sql-formatter", version = "4.0.2" },
+		"stylua",
+		"typescript-language-server",
+	},
+
+	-- if set to true this will check each tool for updates. If updates
+	-- are available the tool will be updated. This setting does not
+	-- affect :MasonToolsUpdate or :MasonToolsInstall.
+	-- Default: false
+	auto_update = false,
+
+	-- automatically install / update on startup. If set to false nothing
+	-- will happen on startup. You can use :MasonToolsInstall or
+	-- :MasonToolsUpdate to install tools and check for updates.
+	-- Default: true
+	run_on_start = true,
+
+	-- set a delay (in ms) before the installation starts. This is only
+	-- effective if run_on_start is set to true.
+	-- e.g.: 5000 = 5 second delay, 10000 = 10 second delay, etc...
+	-- Default: 0
+	start_delay = 3000, -- 3 second delay
+})
 
 -- LSP Setup https://github.com/neovim/nvim-lspconfig
 
 -- Mappings.
 -- See `:help vim.diagnostic.*` for documentation on any of the below functions
-local opts = { noremap=true, silent=true }
-vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
-vim.keymap.set('n', '<space>E', vim.diagnostic.goto_prev, opts)
-vim.keymap.set('n', '<space>e', vim.diagnostic.goto_next, opts)
-vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
+local opts = { noremap = true, silent = true }
+vim.keymap.set("n", "ge", vim.diagnostic.open_float, opts)
+vim.keymap.set("n", "<space>E", function()
+	vim.diagnostic.goto_prev({ severity = vim.diagnostic.severity.ERROR })
+end, opts)
+vim.keymap.set("n", "<space>W", function()
+	vim.diagnostic.goto_prev({ severity = vim.diagnostic.severity.WARN })
+end, opts)
+vim.keymap.set("n", "<space>e", function()
+	vim.diagnostic.goto_next({ severity = vim.diagnostic.severity.ERROR })
+end, opts)
+vim.keymap.set("n", "<space>e", function()
+	vim.diagnostic.goto_prev({ severity = vim.diagnostic.severity.ERROR })
+end, opts)
+vim.keymap.set("n", "<space>q", vim.diagnostic.setloclist, opts)
 
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
 local on_attach = function(client, bufnr)
-  -- Enable completion triggered by <c-x><c-o>
-  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+	-- Enable completion triggered by <c-x><c-o>
+	vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
 
-  -- Mappings.
-  -- See `:help vim.lsp.*` for documentation on any of the below functions
-  local bufopts = { noremap=true, silent=true, buffer=bufnr }
-  vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
-  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
-  vim.keymap.set('n', 'gh', vim.lsp.buf.hover, bufopts)
-  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
-  vim.keymap.set('i', '<C-k>', vim.lsp.buf.signature_help, bufopts)
-  vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
-  vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
-  vim.keymap.set('n', '<space>wl', function()
-    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-  end, bufopts)
-  vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
-  vim.keymap.set('n', '<space>r', vim.lsp.buf.rename, bufopts)
-  vim.keymap.set('n', '<space>,', vim.lsp.buf.code_action, bufopts)
-  vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
-  lsp_status.on_attach(client, bufnr)
+	-- Mappings.
+	-- See `:help vim.lsp.*` for documentation on any of the below functions
+	local bufopts = { noremap = true, silent = true, buffer = bufnr }
+	vim.keymap.set("n", "gD", vim.lsp.buf.declaration, bufopts)
+	vim.keymap.set("n", "gd", vim.lsp.buf.definition, bufopts)
+	vim.keymap.set("n", "gh", vim.lsp.buf.hover, bufopts)
+	vim.keymap.set("n", "gi", vim.lsp.buf.implementation, bufopts)
+	vim.keymap.set("i", "<C-k>", vim.lsp.buf.signature_help, bufopts)
+	vim.keymap.set("n", "<space>wa", vim.lsp.buf.add_workspace_folder, bufopts)
+	vim.keymap.set("n", "<space>wr", vim.lsp.buf.remove_workspace_folder, bufopts)
+	vim.keymap.set("n", "<space>wl", function()
+		print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+	end, bufopts)
+	vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, bufopts)
+	vim.keymap.set("n", "<space>r", vim.lsp.buf.rename, bufopts)
+	vim.keymap.set("n", "<space>,", vim.lsp.buf.code_action, bufopts)
+	vim.keymap.set("n", "gr", vim.lsp.buf.references, bufopts)
 end
 
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
 
 local lsp_options = {
-  on_attach = on_attach,
-  flags = lsp_flags,
-  capabilities
+	on_attach = on_attach,
+	capabilities,
 }
 -- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#tsserver
-require('lspconfig')['tsserver'].setup(lsp_options)
+require("lspconfig")["tsserver"].setup(lsp_options)
 
-require('lspconfig')['rust_analyzer'].setup(lsp_options)
+require("lspconfig")["rust_analyzer"].setup(lsp_options)
 
--- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#graphql
-require('lspconfig').graphql.setup {
-  on_attach = on_attach,
-  flags = flags,
-  capabilities,
-  filetypes = { "graphql", "typescriptreact", "javascriptreact", "typescript" }
-}
-
--- https://github.com/hrsh7th/nvim-cmp
-local cmp = require 'cmp'
-
-cmp.setup({
-  mapping = cmp.mapping.preset.insert({
-    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-f>'] = cmp.mapping.scroll_docs(4),
-    ['<C-Space>'] = cmp.mapping.complete(),
-    ['<C-e>'] = cmp.mapping.abort(),
-    ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-  }),
-  sources = cmp.config.sources({
-    { name = 'nvim_lsp' },
-  }, {
-    { name = 'buffer' },
-  })
+-- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#sumneko_lua
+require("lspconfig").sumneko_lua.setup({
+	settings = {
+		on_attach,
+		capabilities,
+		Lua = {
+			runtime = {
+				-- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+				version = "LuaJIT",
+			},
+			diagnostics = {
+				-- Get the language server to recognize the `vim` global
+				globals = { "vim" },
+			},
+			workspace = {
+				-- Make the server aware of Neovim runtime files
+				library = vim.api.nvim_get_runtime_file("", true),
+			},
+			-- Do not send telemetry data containing a randomized but unique identifier
+			telemetry = {
+				enable = false,
+			},
+		},
+	},
 })
 
+-- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#graphql
+require("lspconfig").graphql.setup({
+	on_attach = on_attach,
+	capabilities,
+	filetypes = { "graphql", "typescriptreact", "javascriptreact", "typescript" },
+})
+
+-- https://github.com/hrsh7th/nvim-cmp
+local cmp = require("cmp")
+
+cmp.setup({
+	snippet = {
+		expand = function(args)
+			vim.fn["vsnip#anonymous"](args.body)
+		end,
+	},
+	mapping = cmp.mapping.preset.insert({
+		["<C-b>"] = cmp.mapping.scroll_docs(-4),
+		["<C-f>"] = cmp.mapping.scroll_docs(4),
+		["<C-Space>"] = cmp.mapping.complete(),
+		["<C-e>"] = cmp.mapping.abort(),
+		["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+	}),
+	sources = cmp.config.sources({
+		{ name = "nvim_lsp" },
+	}, {
+		{ name = "buffer" },
+	}),
+})
 
 -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
-cmp.setup.cmdline({ '/', '?' }, {
-  mapping = cmp.mapping.preset.cmdline(),
-  sources = {
-    { name = 'buffer' }
-  }
+cmp.setup.cmdline({ "/", "?" }, {
+	mapping = cmp.mapping.preset.cmdline(),
+	sources = {
+		{ name = "buffer" },
+	},
 })
 
 -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
-cmp.setup.cmdline(':', {
-  mapping = cmp.mapping.preset.cmdline(),
-  sources = cmp.config.sources({
-    { name = 'path' }
-  }, {
-    { name = 'cmdline' }
-  })
+cmp.setup.cmdline(":", {
+	mapping = cmp.mapping.preset.cmdline(),
+	sources = cmp.config.sources({
+		{ name = "path" },
+	}, {
+		{ name = "cmdline" },
+	}),
 })
 
 -- LSP Lines (https://git.sr.ht/~whynothugo/lsp_lines.nvim#installation)
 require("lsp_lines").setup()
 -- Disable virtual_text since it's redundant due to lsp_lines.
 vim.diagnostic.config({
-  virtual_text = false,
+	virtual_text = false,
 })
 
 -- https://github.com/j-hui/fidget.nvim
-require"fidget".setup{}
+require("fidget").setup({})
--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,38 +1,47 @@
 -- Bootstrap packer, refer https://github.com/wbthomason/packer.nvim#bootstrapping
-vim.cmd [[
+vim.cmd([[
 let data_dir = has('nvim') ? stdpath('data') . '/site' : '~/.vim'
 if empty(glob(data_dir . '/autoload/plug.vim'))
   silent execute '!curl -fLo '.data_dir.'/autoload/plug.vim --create-dirs  https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
   autocmd VimEnter * PlugInstall --sync | source $MYVIMRC
 endif
-]]
+]])
 
-local Plug = vim.fn['plug#']
-vim.call('plug#begin', '~/.config/nvim/plugged')
-Plug 'tpope/vim-commentary'
-Plug 'tpope/vim-fugitive'
-Plug 'tpope/vim-surround'
-Plug 'nvim-lua/plenary.nvim'
-Plug 'windwp/nvim-spectre'
-Plug 'nvim-telescope/telescope.nvim'
-Plug 'kyazdani42/nvim-web-devicons'
-Plug 'sindrets/diffview.nvim'
-Plug('nvim-treesitter/nvim-treesitter', {['do'] = ':TSUpdate'})
-Plug 'windwp/nvim-ts-autotag'
-Plug 'windwp/nvim-autopairs'
-Plug 'hoob3rt/lualine.nvim'
-Plug 'alvarosevilla95/luatab.nvim'
-Plug 'Mofiqul/vscode.nvim'
-Plug('iamcco/markdown-preview.nvim', {['do'] = 'cd app && npm install'})
-Plug 'sbdchd/neoformat'
-Plug 'lewis6991/gitsigns.nvim'
-Plug 'neovim/nvim-lspconfig'
-Plug 'hrsh7th/cmp-nvim-lsp'
-Plug 'hrsh7th/cmp-buffer'
-Plug 'hrsh7th/cmp-path'
-Plug 'hrsh7th/cmp-cmdline'
-Plug 'hrsh7th/nvim-cmp'
-Plug 'kyazdani42/nvim-tree.lua'
-Plug 'https://git.sr.ht/~whynothugo/lsp_lines.nvim'
-Plug 'j-hui/fidget.nvim'
-vim.call('plug#end')
+local Plug = vim.fn["plug#"]
+vim.call("plug#begin", "~/.config/nvim/plugged")
+Plug("tpope/vim-commentary")
+Plug("tpope/vim-fugitive")
+Plug("tpope/vim-surround")
+Plug("nvim-lua/plenary.nvim")
+Plug("windwp/nvim-spectre")
+Plug("nvim-telescope/telescope.nvim")
+Plug("kyazdani42/nvim-web-devicons")
+Plug("sindrets/diffview.nvim")
+Plug("nvim-treesitter/nvim-treesitter", { ["do"] = ":TSUpdate" })
+Plug("windwp/nvim-ts-autotag")
+Plug("windwp/nvim-autopairs")
+Plug("hoob3rt/lualine.nvim")
+Plug("alvarosevilla95/luatab.nvim")
+Plug("Mofiqul/vscode.nvim")
+Plug("iamcco/markdown-preview.nvim", { ["do"] = "cd app && npm install" })
+Plug("sbdchd/neoformat")
+Plug("lewis6991/gitsigns.nvim")
+Plug("kyazdani42/nvim-tree.lua")
+
+-- LSP
+Plug("neovim/nvim-lspconfig")
+Plug("hrsh7th/cmp-nvim-lsp")
+Plug("hrsh7th/cmp-buffer")
+Plug("hrsh7th/cmp-path")
+Plug("hrsh7th/cmp-cmdline")
+Plug("hrsh7th/nvim-cmp")
+Plug("hrsh7th/cmp-vsnip")
+Plug("hrsh7th/vim-vsnip")
+
+Plug("https://git.sr.ht/~whynothugo/lsp_lines.nvim")
+Plug("j-hui/fidget.nvim")
+
+Plug("williamboman/mason.nvim")
+Plug("williamboman/mason-lspconfig.nvim")
+Plug("WhoIsSethDaniel/mason-tool-installer.nvim")
+vim.call("plug#end")
