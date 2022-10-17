fc8574dd364719e48b2d84d5a4407796195cb607
Wong Jia Hau <hou32hou@gmail.com>
2022-10-17 04:09:30 +0000

    fix(nvim/cmp_nvim_lsp): remove deprecated usage
    Refer https://github.com/hrsh7th/cmp-nvim-lsp/issues/38#issuecomment-1279744539

--- nvim/init.lua
+++ nvim/init.lua
@@ -235,7 +235,7 @@ local on_attach = function(client, bufnr)
 	vim.keymap.set("n", "gr", vim.lsp.buf.references, bufopts)
 end
 
-local capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
 local lsp_options = {
 	on_attach = on_attach,
