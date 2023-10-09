d579f9ce63b2e9316916e13e8f9c9096e26e1e85
wongjiahau <hou32hou@gmail.com>
2023-10-09 08:16:09 +0000

    fix(nvim/lsp): gh,gd not working for some projects
    I fixed it by updating the LSP config using the latest example found on
    https://github.com/neovim/nvim-lspconfig.

--- nvim/init.lua
+++ nvim/init.lua
@@ -113,6 +113,10 @@ o.inccommand = "nosplit" -- For viewing live substitution
 o.hidden = true -- to ensure terminal remains alive
 o.relativenumber = true
 
+-- Refer https://stackoverflow.com/questions/7912060/vim-how-can-i-open-a-file-at-right-side-as-vsplit-from-a-left-side-nerdtree-pan
+o.splitbelow = true
+o.splitright = true
+
 -- grep
 vim.cmd([[
 set grepprg=rg\ --vimgrep\ --no-heading\ --smart-case
@@ -220,29 +224,32 @@ vim.keymap.set("n", "<space>w", function()
 end, opts)
 vim.keymap.set("n", "<space>q", vim.diagnostic.setloclist, opts)
 
--- Use an on_attach function to only map the following keys
+-- Use LspAttach autocommand to only map the following keys
 -- after the language server attaches to the current buffer
-local on_attach = function(client, bufnr)
-	-- Enable completion triggered by <c-x><c-o>
-	vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
+vim.api.nvim_create_autocmd("LspAttach", {
+	group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+	callback = function(ev)
+		-- Enable completion triggered by <c-x><c-o>
+		vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
 
-	-- Mappings.
-	-- See `:help vim.lsp.*` for documentation on any of the below functions
-	local bufopts = { noremap = true, silent = true, buffer = bufnr }
-	vim.keymap.set("n", "gD", vim.lsp.buf.declaration, bufopts)
-	vim.keymap.set("n", "gd", vim.lsp.buf.definition, bufopts)
-	vim.keymap.set("n", "gh", vim.lsp.buf.hover, bufopts)
-	vim.keymap.set("n", "gi", vim.lsp.buf.implementation, bufopts)
-	vim.keymap.set("i", "<C-k>", vim.lsp.buf.signature_help, bufopts)
-	vim.keymap.set("n", "<space>wa", vim.lsp.buf.add_workspace_folder, bufopts)
-	vim.keymap.set("n", "<space>wr", vim.lsp.buf.remove_workspace_folder, bufopts)
-	vim.keymap.set("n", "<space>wl", function()
-		print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-	end, bufopts)
-	vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, bufopts)
-	vim.keymap.set("n", "<space>r", vim.lsp.buf.rename, bufopts)
-	vim.keymap.set("n", "gr", vim.lsp.buf.references, bufopts)
-end
+		-- Buffer local mappings.
+		-- See `:help vim.lsp.*` for documentation on any of the below functions
+		local opts = { buffer = ev.buf }
+		vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+		vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+		vim.keymap.set("n", "gh", vim.lsp.buf.hover, opts)
+		vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+		vim.keymap.set("i", "<C-k>", vim.lsp.buf.signature_help, opts)
+		vim.keymap.set("n", "<space>wa", vim.lsp.buf.add_workspace_folder, opts)
+		vim.keymap.set("n", "<space>wr", vim.lsp.buf.remove_workspace_folder, opts)
+		vim.keymap.set("n", "<space>wl", function()
+			print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+		end, opts)
+		vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, opts)
+		vim.keymap.set("n", "<space>r", vim.lsp.buf.rename, opts)
+		vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+	end,
+})
 
 local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
@@ -252,7 +259,6 @@ local lsp_options = {
 }
 -- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#tsserver
 require("lspconfig")["tsserver"].setup({
-	on_attach,
 	capabilities,
 	init_options = {
 		preferences = {
@@ -263,7 +269,6 @@ require("lspconfig")["tsserver"].setup({
 })
 
 require("lspconfig")["rust_analyzer"].setup({
-	on_attach = on_attach,
 	capabilities,
 	settings = {
 		["rust-analyzer"] = {
@@ -277,7 +282,6 @@ require("lspconfig")["rust_analyzer"].setup({
 -- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#sumneko_lua
 require("lspconfig").lua_ls.setup({
 	settings = {
-		on_attach,
 		capabilities,
 		Lua = {
 			runtime = {
@@ -302,25 +306,12 @@ require("lspconfig").lua_ls.setup({
 
 -- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#graphql
 require("lspconfig").graphql.setup({
-	on_attach = on_attach,
 	capabilities,
 	filetypes = { "graphql", "typescriptreact", "javascriptreact", "typescript" },
 })
-
--- https://github.com/lighttiger2505/sqls
-require("lspconfig").sqls.setup({
-	on_attach,
+-- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#sqlls
+require("lspconfig").sqlls.setup({
 	capabilities,
-	settings = {
-		sqls = {
-			connections = {
-				{
-					driver = "postgresql",
-					dataSourceName = "host=127.0.0.1 port=5432 user=postgres password='' dbname=postgres sslmode=disable",
-				},
-			},
-		},
-	},
 })
 
 -- https://github.com/hrsh7th/nvim-cmp
