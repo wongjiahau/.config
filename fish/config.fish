58481cb6e2a352170ea5b16b379b6f8ebc0adf69
wongjiahau <hou32hou@gmail.com>
2025-10-26 14:47:59 +0800

    feat(config.fish): add direnv and Nix

--- fish/config.fish
+++ fish/config.fish
@@ -2,7 +2,9 @@ fish_default_key_bindings
 set -gx PATH $HOME/.cargo/bin $PATH
 set -gx PATH /opt/homebrew/bin $PATH
 set -gx PATH /usr/local/go/bin $PATH
+set -gx PATH $HOME/go/bin $PATH
 set -gx PATH $GOPATH/bin $PATH
+set -gx EDITOR ki
 
 set -x PATH ~/npm/bin:$PATH
 
@@ -48,5 +50,34 @@ else
         start_agent
     end
 end
+
+# pnpm
+set -gx PNPM_HOME /Users/wongjiahau/Library/pnpm
+set -gx PATH "$PNPM_HOME" $PATH
+# pnpm end
+
+# Setting PATH for Python 3.10
+# The original version is saved in /Users/wongjiahau/.config/fish/config.fish.pysave
+set -x PATH "/Library/Frameworks/Python.framework/Versions/3.10/bin" "$PATH"
+
+zoxide init fish | source
+
+# bun
+set --export BUN_INSTALL "$HOME/.bun"
+set --export PATH $BUN_INSTALL/bin $PATH
+
 # Set up fzf key bindings
 fzf --fish | source
+
+# Setting up nix-shell
+if test -e /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
+    set -gx PATH /nix/var/nix/profiles/default/bin $PATH
+    set -gx NIX_PATH $HOME/.nix-defexpr/channels
+    set -gx NIX_PROFILES "/nix/var/nix/profiles/default $HOME/.nix-profile"
+    set -gx NIX_SSL_CERT_FILE /nix/var/nix/profiles/default/etc/ssl/certs/ca-bundle.crt
+    set -gx PATH $HOME/.nix-profile/bin $PATH
+end
+
+
+direnv hook fish | source
+
